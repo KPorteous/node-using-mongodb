c4b81aa46d860c4876d942833a0427854fda4277
KPorteous <kieran.porteous@groubook.co.uk>
2022-11-08 13:19:49 +0000

    finished module 2

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import mongoose, { mongo } from 'mongoose';
 
 const app = express();
 const PORT = 4000;
@@ -7,6 +8,12 @@ const PORT = 4000;
 // bodyparser setup
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); 
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/productsdb', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.get('/', (req, res) =>
     res.send(`Store server running on port ${PORT}`)
